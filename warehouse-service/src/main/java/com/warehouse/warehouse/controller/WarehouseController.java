cc3e201183f310021dcc2a66801765c041bbbf9d
Dominik <dominikgzd@gmail.com>
2021-02-11 12:21:53 +0100

    Add getWarehouses, updateWarehouse, deleteWarehouse, and createErrorMessage methods. Add Validation

--- warehouse-service/src/main/java/com/warehouse/warehouse/controller/WarehouseController.java
+++ warehouse-service/src/main/java/com/warehouse/warehouse/controller/WarehouseController.java
@@ -4,10 +4,13 @@ import com.warehouse.warehouse.common.ResponseTemplate;
 import com.warehouse.warehouse.entity.Warehouse;
 import com.warehouse.warehouse.service.WarehouseService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/warehouse/")
@@ -19,8 +22,15 @@ public class WarehouseController {
         this.warehouseService = warehouseService;
     }
 
+    @GetMapping("")
+    public List<Warehouse> getWarehouses(){
+        return warehouseService.getWarehouses();
+    }
+
     @PostMapping("")
-    public ResponseEntity<Warehouse> saveWarehouse(@RequestBody Warehouse warehouse){
+    public ResponseEntity<Warehouse> saveWarehouse(@Valid @RequestBody Warehouse warehouse, BindingResult errors){
+        if(errors.hasErrors())
+            warehouseService.createErrorsMessage(errors);
        Warehouse savedWarehouse = warehouseService.saveWarehouse(warehouse);
         URI location = ServletUriComponentsBuilder.fromCurrentRequest()
                 .path("/{id}")
@@ -32,5 +42,20 @@ public class WarehouseController {
     @GetMapping("{id}")
     public ResponseTemplate getWarehouseWithAddress(@PathVariable Long id){
         return warehouseService.getWarehouseWithAddress(id);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Warehouse> updateWarehouse(@PathVariable Long id,@Valid @RequestBody Warehouse warehouse, BindingResult errors){
+        if(errors.hasErrors())
+            warehouseService.createErrorsMessage(errors);
+        Warehouse updatedWarehouse = warehouseService.updateWarehouse(warehouse, id);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .build().toUri();
+        return ResponseEntity.created(location).body(updatedWarehouse);
+    }
+
+    @DeleteMapping("{id}")
+    public Warehouse deleteWarehouse(@PathVariable Long id){
+        return warehouseService.deleteWarehouse(id);
     }
 }
