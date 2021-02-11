623256f278e8bb32f305db0839952390944a9127
Dominik <dominikgzd@gmail.com>
2021-02-11 12:24:23 +0100

    Add getWarehouses, updateWarehouse, deleteWarehouse, and createErrorMessage methods.

--- warehouse-service/src/main/java/com/warehouse/warehouse/service/WarehouseService.java
+++ warehouse-service/src/main/java/com/warehouse/warehouse/service/WarehouseService.java
@@ -6,8 +6,13 @@ import com.warehouse.warehouse.entity.Warehouse;
 import com.warehouse.warehouse.repository.WarehouseRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class WarehouseService {
@@ -20,6 +25,10 @@ public class WarehouseService {
         this.restTemplate = restTemplate;
     }
 
+    public List<Warehouse> getWarehouses() {
+        return warehouseRepository.findAll();
+    }
+
     public Warehouse saveWarehouse(Warehouse warehouse) {
         if(warehouse.getWarehouseId() != null){
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"Warehouse cannot have id");
@@ -29,11 +38,37 @@ public class WarehouseService {
 
     public ResponseTemplate getWarehouseWithAddress(Long id) {
         ResponseTemplate template = new ResponseTemplate();
-        Warehouse warehouse = warehouseRepository.findByWarehouseId(id);
+        Warehouse warehouse = warehouseRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Cannot find warehouse with provided id"));
 
         Address address = restTemplate.getForObject("http://ADDRESS-SERVICE/api/address/"+warehouse.getAddressId(), Address.class);
         template.setWarehouse(warehouse);
         template.setAddress(address);
         return template;
+    }
+
+    public Warehouse updateWarehouse(Warehouse warehouse, Long id) {
+        if(!warehouse.getWarehouseId().equals(id))
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "Warehouse id must be same as id in path variable");
+        Warehouse oldWarehouse = warehouseRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Cannot find warehouse with provided id"));
+        return warehouseRepository.save(warehouse);
+    }
+
+    public Warehouse deleteWarehouse(Long id){
+        Warehouse warehouseToDelete = warehouseRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Cannot find warehouse with provided id"));
+        warehouseRepository.delete(warehouseToDelete);
+        return warehouseToDelete;
+    }
+
+    public void createErrorsMessage(BindingResult errors){
+        List<ObjectError> errorList = errors.getAllErrors();
+        String message = errorList.stream()
+                .map(ObjectError::getDefaultMessage)
+                .map(err -> err +" ")
+                .map(String::toString)
+                .collect(Collectors.joining());
+        throw new ResponseStatusException(HttpStatus.BAD_REQUEST,message);
     }
 }
