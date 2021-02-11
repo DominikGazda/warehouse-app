4b56a5983fe60c8447ce3fb21d213fc1fb09154a
Dominik <dominikgzd@gmail.com>
2021-02-11 13:39:15 +0100

    Add updateClient, deleteClient and createErrorMessages methods

--- client-service/src/main/java/com/warehouse/client/service/ClientService.java
+++ client-service/src/main/java/com/warehouse/client/service/ClientService.java
@@ -4,9 +4,12 @@ import com.warehouse.client.entity.Client;
 import com.warehouse.client.repository.ClientRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ClientService {
@@ -31,5 +34,28 @@ public class ClientService {
     public Client getClientById(Long id) {
         return clientRepository.findById(id)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST,"Cannot find client with provided id"));
+    }
+
+    public Client updateClient(Client client, Long id) {
+        if(!client.getIdClient().equals(id))
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "Category id must be same as id in path variable");
+        return clientRepository.save(client);
+    }
+
+    public Client deleteClient(Long id) {
+        Client clientToDelete = clientRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST,"Cannot find client with provided id"));
+        clientRepository.delete(clientToDelete);
+        return clientToDelete;
+    }
+
+    public void createErrorsMessage(BindingResult errors){
+        List<ObjectError> errorList = errors.getAllErrors();
+        String message = errorList.stream()
+                .map(ObjectError::getDefaultMessage)
+                .map(err -> err +" ")
+                .map(String::toString)
+                .collect(Collectors.joining());
+        throw new ResponseStatusException(HttpStatus.BAD_REQUEST,message);
     }
 }
