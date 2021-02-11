8ca2bf64d461a4a528b1243a79ab38991ec0aa0e
Dominik <dominikgzd@gmail.com>
2021-02-11 22:37:15 +0100

    Add updateOrder, changeOrderStatus, createErrorsMessage and deleteOrder methods.

--- order-service/src/main/java/com/warehouse/order/service/OrderService.java
+++ order-service/src/main/java/com/warehouse/order/service/OrderService.java
@@ -7,10 +7,13 @@ import com.warehouse.order.entity.Order;
 import com.warehouse.order.repository.OrderRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class OrderService {
@@ -30,6 +33,7 @@ public class OrderService {
     public Order saveOrder(Order order) {
         if (order.getOrderId() != null)
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"Order cannot have id");
+        order.setActive(true);
         return  orderRepository.save(order);
     }
 
@@ -41,4 +45,39 @@ public class OrderService {
                 RequestTemplate.class);
         return new ResponseTemplate(order.getOrderId(), client, requestTemplate.getProduct(), order.getDate(), order.isActive());
     }
+
+
+    public Order updateOrder(Order order, Long id) {
+        if(!order.getOrderId().equals(id))
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "Order id must be same as id in path variable");
+        Order oldOrder = orderRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.CONFLICT, "Cannot find order with provided id"));
+        return orderRepository.save(order);
+    }
+
+    public Order changeOrderStatus(Long id) {
+        Order order = orderRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST,"Cannot  find order with provided id"));
+        order.setActive(!order.isActive());
+        return orderRepository.save(order);
+    }
+
+    public Order deleteOrder(Long id) {
+        Order orderToDelete = orderRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.CONFLICT, "Cannot find order with provided id"));
+        orderRepository.delete(orderToDelete);
+        return orderToDelete;
+    }
+
+    public void createErrorsMessage(BindingResult errors){
+        List<ObjectError> errorList = errors.getAllErrors();
+        String message = errorList.stream()
+                .map(ObjectError::getDefaultMessage)
+                .map(err -> err +" ")
+                .map(String::toString)
+                .collect(Collectors.joining());
+        throw new ResponseStatusException(HttpStatus.BAD_REQUEST,message);
+    }
+
+
 }
