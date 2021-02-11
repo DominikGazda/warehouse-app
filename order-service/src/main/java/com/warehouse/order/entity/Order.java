1f094d3f553c2c22bf4b7f1f90ef573d535fb860
Dominik <dominikgzd@gmail.com>
2021-02-11 22:36:33 +0100

    Add validation annotations

--- order-service/src/main/java/com/warehouse/order/entity/Order.java
+++ order-service/src/main/java/com/warehouse/order/entity/Order.java
@@ -1,9 +1,11 @@
 package com.warehouse.order.entity;
 
+import com.warehouse.order.constraint.DateTimeFormatterConstraint;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "orders")
@@ -16,12 +18,14 @@ public class Order {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long orderId;
 
+    @NotNull(message = "{com.warehouse.order.entity.Order.clientId.NotNull}")
     private Long clientId;
 
+    @NotNull(message = "{com.warehouse.order.entity.Order.productId.NotNull}")
     private Long productId;
 
+    @DateTimeFormatterConstraint
     private String date;
 
     private boolean active;
-
 }
