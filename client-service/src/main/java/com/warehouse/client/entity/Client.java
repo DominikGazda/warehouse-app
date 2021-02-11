b59e6e85d39b96bc2d2bad34cc4cca0ae3a57ecf
Dominik <dominikgzd@gmail.com>
2021-02-11 13:38:24 +0100

    Add validation annotations

--- client-service/src/main/java/com/warehouse/client/entity/Client.java
+++ client-service/src/main/java/com/warehouse/client/entity/Client.java
@@ -1,10 +1,12 @@
 package com.warehouse.client.entity;
 
+import com.warehouse.client.constraint.PhoneNumberFormatter;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 @Table(name="client")
@@ -17,12 +19,15 @@ public class Client {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idClient;
 
+    @NotBlank(message = "{com.warehouse.client.entity.Client.name.NotBlank}")
     private String name;
 
+    @NotBlank(message = "{com.warehouse.client.entity.Client.lastName.NotBlank}")
     private String lastName;
 
+    @NotBlank(message = "{com.warehouse.client.entity.Client.address.NotBlank}")
     private String address;
 
+    @PhoneNumberFormatter
     private String phoneNumber;
-
 }
