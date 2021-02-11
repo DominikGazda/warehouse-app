f019490d09d5556a645e74df4af08eb3e8a1dda0
Dominik <dominikgzd@gmail.com>
2021-02-11 13:09:02 +0100

    Add updateProduct and deleteProduct, createErrorsMessage methods.

--- product-service/src/main/java/com/warehouse/product/service/ProductService.java
+++ product-service/src/main/java/com/warehouse/product/service/ProductService.java
@@ -7,10 +7,13 @@ import com.warehouse.product.entity.Product;
 import com.warehouse.product.repository.ProductRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ProductService {
@@ -41,5 +44,30 @@ public class ProductService {
         Category category = restTemplate.getForObject("http://CATEGORY-SERVICE/api/category/"+product.getProductId(),
                 Category.class);
         return new ResponseTemplate(product, requestTemplate.getWarehouse(), requestTemplate.getAddress(), category);
+    }
+
+    public Product updateProduct(Product product, Long id) {
+        if(!product.getProductId().equals(id))
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "Category id must be same as id in path variable");
+        Product oldProduct = productRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.CONFLICT, "Cannot find product with provided id"));
+        return productRepository.save(product);
+    }
+
+    public Product deleteProduct(Long id) {
+        Product productToDelete = productRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.CONFLICT, "Cannot find product with provided id"));
+        productRepository.delete(productToDelete);
+        return productToDelete;
+    }
+
+    public void createErrorsMessage(BindingResult errors){
+        List<ObjectError> errorList = errors.getAllErrors();
+        String message = errorList.stream()
+                .map(ObjectError::getDefaultMessage)
+                .map(err -> err +" ")
+                .map(String::toString)
+                .collect(Collectors.joining());
+        throw new ResponseStatusException(HttpStatus.BAD_REQUEST,message);
     }
 }
