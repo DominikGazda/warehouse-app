ea7a6898d913d6840dc8230c23f60c87236161b2
Dominik <dominikgzd@gmail.com>
2021-02-11 11:30:15 +0100

    Add updateAddress and createErrorMessage methods

--- address-service/src/main/java/com/warehouse/address/service/AddressService.java
+++ address-service/src/main/java/com/warehouse/address/service/AddressService.java
@@ -4,9 +4,12 @@ import com.warehouse.address.entity.Address;
 import com.warehouse.address.repository.AddressRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class AddressService {
@@ -23,13 +26,31 @@ public class AddressService {
 
     public Address saveAddress(Address address) {
         if(address.getAddressId() != null){
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"Address cannot have id");
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Address cannot have id");
         }
         return addressRepository.save(address);
     }
 
     public Address getAddressById(Long id) {
         return addressRepository.findById(id)
-                .orElseThrow(()-> new ResponseStatusException(HttpStatus.BAD_REQUEST,"Address not found"));
+                .orElseThrow(()-> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Address not found"));
+    }
+
+    public Address updateAddress(Address address, Long id) {
+        if(!address.getAddressId().equals(id))
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "Address id must be same as id in path variable");
+        Address oldAddress = addressRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Address not found"));
+        return addressRepository.save(address);
+    }
+
+    public void createErrorsMessage(BindingResult errors){
+        List<ObjectError> errorList = errors.getAllErrors();
+        String message = errorList.stream()
+                .map(ObjectError::getDefaultMessage)
+                .map(err -> err +" ")
+                .map(String::toString)
+                .collect(Collectors.joining());
+        throw new ResponseStatusException(HttpStatus.BAD_REQUEST,message);
     }
 }
